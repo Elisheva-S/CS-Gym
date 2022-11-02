820f148fe9c15d60060f889800f7023d1032e8d9
Elisheva-S <105245801+Elisheva-S@users.noreply.github.com>
2022-11-02 15:16:17 +0200

    Add files via upload

---
+++ Foreach.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS_Concepts
+{
+    internal class Foreach
+    {
+        public void Run()
+        {
+            int sum = 0;
+            int[] arr = new int[10];
+            foreach (var item in arr)
+            {
+                sum += item;
+            }
+            for (int i = 0; i < arr.Length; i++)
+            {
+                sum += arr[i];
+            }
+        }
+        public void RunMyItemsArray()
+        {
+            Items items = new Items();
+            string allStrings = "";
+            foreach (var item in items)
+            {
+                allStrings+=item.ToString();
+            }
+        }
+    }
+    public class Items : IEnumerable,IEnumerator
+    {
+        public string[] arr = { "ball", "drink", "bag" };
+        int id = -1;
+        public object Current
+        {
+            get { return arr[id]; }
+        }
+
+        public IEnumerator GetEnumerator()
+        {
+            return this;
+        }
+
+        public bool MoveNext()
+        {
+            if (id<arr.Length)
+            {
+                id++;
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public void Reset()
+        {
+            id = -1;
+        }
+
+
+        
+    }
+}
+
---
+++ Interfaces.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS_Concepts
+{
+    public interface ITax
+    {
+         int MAAM(int price);
+        int IncomminTax(int price);
+    }
+    public interface INewTax:ITax
+    {
+
+    }
+    public class ImplementInterfaces : ITax, INewTax
+    {
+        public int IncomminTax(int price)
+        {
+            return price;
+        }
+
+        public int MAAM(int price)
+        {
+            return price;
+        }
+    }
+    public class Eilat : ITax
+    {
+        public int IncomminTax(int price)
+        {
+            return 0;
+        }
+
+        public int MAAM(int price)
+        {
+            return 0;
+        }
+    }
+    public class Regular : ITax
+    {
+        public int IncomminTax(int price)
+        {
+            return price*2;
+        }
+
+        public int MAAM(int price)
+        {
+            return price*2;
+        }
+    }
+    public class InterfacesSample
+    {
+        public void Run()
+        {
+            ITax[] arr = new ITax[2];
+            arr[0] = new Eilat();
+            arr[1] = new Regular();
+            arr[0].MAAM(10);
+            arr[0].IncomminTax(10);
+            Test(arr[0]);
+            Eilat eilat = new Eilat();
+            Test(eilat);
+        }
+        public void Test(ITax t)
+        {
+
+        }
+        
+    }
+}
