fdd0a6f51b3013857c0a1956a57e2a13bdba98ab
Elisheva-S <105245801+Elisheva-S@users.noreply.github.com>
2022-11-02 12:39:46 +0200

    Add files via upload

---
+++ Delegate.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS_Concepts
+{
+    internal class Delegate
+    {
+        public delegate int Calculations(int a, int b);
+        public void Run()
+        {
+            Calculations c;
+            c = Calc1;
+            int a = c(1,2);
+            c = Calc3;
+            a=c(3,4);
+            CalcTax(200, Calc1);
+            CalcTax(200, Calc3);
+            CalcTax(200, Calc2);
+        }
+        public int CalcTax(int price,Calculations c)
+        {
+            return (c(price,2));
+        }
+        public int Calc1(int a,int b)
+        {
+            return a + b; 
+        }
+        public int Calc2(int a, int b)
+        {
+            return a * b;
+        }
+        public int Calc3(int a, int b)
+        {
+            return a - b;
+        }
+    }
+}
---
+++ Events.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS_Concepts
+{
+    internal class Events
+    {
+        public delegate void TimeOutDel(int time);
+        public event TimeOutDel TimeOutHandler;
+        public void Run()
+        {
+            //broadcast events to all listeners
+            if(TimeOutHandler != null)
+                TimeOutHandler(1200);
+        }
+    }
+    public class Operate
+    {
+        Events events=new Events();
+        DB db=new DB();
+        UI ui=new UI();
+        public void Init()
+        {
+            events.TimeOutHandler += db.UpdateDBAfterTimeOut;
+            events.TimeOutHandler += ui.UpdateUIAfterTimeOut;
+            events.Run();
+            //unregister from broadcast list
+            events.TimeOutHandler -= ui.UpdateUIAfterTimeOut;
+            events.Run();
+        }
+    public class DB
+    {
+        public void UpdateDBAfterTimeOut(int time)
+        {
+
+        }
+    }
+    public class UI
+    {
+        public void UpdateUIAfterTimeOut(int time)
+        {
+
+        }
+    }
+}
